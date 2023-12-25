1658283d8618282b8d963a4a48fef79d43e684d2
Jhon-crypt <johnoladele690@gmail.com>
2023-12-25 01:43:39 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import Header from './components/header/header'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <Header />
+      </div>
+    </>
+  )
+}
+
+export default App
